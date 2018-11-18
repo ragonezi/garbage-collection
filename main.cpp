3a5331dbc9d3486cfce4ab8154122c0e739e6e45
Raphael-FR <44642837+Raphael-FR@users.noreply.github.com>
2018-11-18 21:25:50 -0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "user.h"
+
+int main ()
+{
+	int opc=0;
+
+	std::cout << "SISTEMA DE GESTÃO DE RESÍDUOS" << std::endl;
+	std::cout << "1.Login" << std::endl;
+	std::cout << "2.Cadastro \n" << std::endl;
+	std::cout << "Digite a opcao desejada: " << std::endl;
+	std::cin >> opc;
+
+	if (opc==1)
+	{
+		/* chamar login: */
+	}
+	else
+	{ 
+		usuario a;	
+		a.set_nome();
+		a.set_CPF();
+		std::cout << "o usuario a e:" << std::endl;
+		std::cout << a.get_nome() << "/" << a.get_CPF() << std::endl;
+	}
+	
+	return 0;
+}
