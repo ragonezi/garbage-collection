38f2f3d1ecad885df4be82835e91405f8dda07b0
25LucasAnselmo <lucasmartins.m.25@gmail.com>
2018-11-24 18:53:41 -0200

    #001 Adiciona o arquivo .sql com as tabelas do banco. Pretendo montar um arquivo com uma explicação simples sobre elas. Também enviei a configuração que utilizei no vscode, a dll do connector do banco de dados e um teste básico no main. Pretendo também criar um mini tutorial dos passos que tomei pra instalar na minha máquina.

--- main.cpp
+++ main.cpp
@@ -1,28 +1,56 @@
+#include <stdlib.h>
 #include <iostream>
 #include "user.h"
+#include "mysql.h"
 
-int main ()
+int main(void)
 {
-	int opc=0;
+	MYSQL mysql;
+
+	if (mysql_init(&mysql) == NULL)
+	{
+		printf("\nInitialization error\n");
+		std::cout << "Press enter to continue!\n";
+		system("PAUSE");
+		return 0;
+	}
+
+	if (mysql_real_connect(&mysql, "localhost", "root", "teste", NULL, 0, NULL, 0) == NULL)
+	{
+		fprintf(stderr, "%s\n", mysql_error(&mysql));
+		mysql_close(&mysql);
+		system("PAUSE");
+		exit(1);
+	}
+
+	printf("Client version: %s", mysql_get_client_info());
+	printf("\nServer version: %s", mysql_get_server_info(&mysql));
+	mysql_close(&mysql);
+	system("PAUSE");
+	return 1;
+}
+
+void tela_inicial()
+{
+	int opc = 0;
 
 	std::cout << "SISTEMA DE GESTÃO DE RESÍDUOS" << std::endl;
 	std::cout << "1.Login" << std::endl;
-	std::cout << "2.Cadastro \n" << std::endl;
+	std::cout << "2.Cadastro \n"
+			  << std::endl;
 	std::cout << "Digite a opcao desejada: " << std::endl;
 	std::cin >> opc;
 
-	if (opc==1)
+	if (opc == 1)
 	{
 		/* chamar login: */
 	}
 	else
-	{ 
-		usuario a;	
+	{
+		usuario a;
 		a.set_nome();
 		a.set_CPF();
 		std::cout << "o usuario a e:" << std::endl;
 		std::cout << a.get_nome() << "/" << a.get_CPF() << std::endl;
 	}
-	
-	return 0;
 }
